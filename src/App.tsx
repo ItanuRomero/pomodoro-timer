7e6a94545c765fc7ac1d94c745d5645176fe08a7
ItanuRomero <itanromero2016@gmail.com>
2024-02-17 15:17:54 -0300

    feat: add themes, types and styles with styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
+import { ThemeProvider } from 'styled-components'
 import { Button } from './components/button'
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/global'
 
 export function App() {
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
       <h1>pomodoro timer</h1>
-
       <Button variant="primary" />
       <Button variant="secondary" />
-    </>
+    </ThemeProvider>
   )
 }
--- src/components/button/styles.ts
+++ src/components/button/styles.ts
@@ -1,4 +1,4 @@
-import styled, { css } from 'styled-components'
+import styled from 'styled-components'
 
 export type ButtonVariant = 'primary' | 'secondary'
 
@@ -6,16 +6,13 @@ interface ButtonContainerProps {
   variant: ButtonVariant
 }
 
-const variants = {
-  primary: 'green',
-  secondary: 'red',
-}
-
 export const ButtonContainer = styled.button<ButtonContainerProps>`
   width: 100px;
   height: 40px;
+  border-radius: 4px;
+  border: 0;
+  margin: 8px;
 
-  ${(props) => css`
-    background-color: ${variants[props.variant]};
-  `}
+  background-color: ${(props) => props.theme['green-500']};
+  color: ${(props) => props.theme.white};
 `
