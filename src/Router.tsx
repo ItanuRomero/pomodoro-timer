a31438fbc9c9d795dd663bf6f2427ab726e41b3e
ItanuRomero <itanromero2016@gmail.com>
2024-02-18 22:00:12 -0300

    feat: create layout and add header

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,15 +1,21 @@
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import { Home } from './pages/Home'
 import { About } from './pages/About'
+import { DefaultLayout } from './layouts/DefaultLayout'
 
 const router = createBrowserRouter([
   {
-    path: '/',
-    element: <Home />,
-  },
-  {
-    path: '/about',
-    element: <About />,
+    element: <DefaultLayout />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: '/about',
+        element: <About />,
+      },
+    ],
   },
 ])
 
---
+++ src/components/Header/styles.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+
+export const HeaderContainer = styled.header`
+  width: 100%;
+  height: 48px;
+  background-color: ${(props) => props.theme['gray-700']};
+`
