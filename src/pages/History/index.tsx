38ed03992c5856029b9f58581a15e0392911a4b2
ItanuRomero <itanromero2016@gmail.com>
2024-03-09 16:58:49 -0300

    feat: list cycles on history page

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,13 +1,14 @@
 import { useContext } from 'react'
 import { HistoryContainer, Status, TableContainer } from './styles'
 import { CycleContext } from '../Home/Context/cycles'
+import { formatDistanceToNow } from 'date-fns'
+import { ptBR } from 'date-fns/locale'
 
 export function History() {
   const { cycles } = useContext(CycleContext)
   return (
     <HistoryContainer>
       <h2>Meu histórico</h2>
-      <pre>{JSON.stringify(cycles)}</pre>
 
       <TableContainer>
         <table>
@@ -20,46 +21,31 @@ export function History() {
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>Um nome ae</td>
-              <td>25m</td>
-              <td>Há cerca de 2meses</td>
-              <td>
-                <Status color="green">Concluído</Status>
-              </td>
-            </tr>
-            <tr>
-              <td>Um nome ae</td>
-              <td>25m</td>
-              <td>Há cerca de 2meses</td>
-              <td>
-                <Status color="green">Concluído</Status>
-              </td>
-            </tr>
-            <tr>
-              <td>Um nome ae</td>
-              <td>25m</td>
-              <td>Há cerca de 2meses</td>
-              <td>
-                <Status color="green">Concluído</Status>
-              </td>
-            </tr>
-            <tr>
-              <td>Um nome ae</td>
-              <td>25m</td>
-              <td>Há cerca de 2meses</td>
-              <td>
-                <Status color="green">Concluído</Status>
-              </td>
-            </tr>
-            <tr>
-              <td>Um nome ae</td>
-              <td>15m</td>
-              <td>Há cerca de 2meses</td>
-              <td>
-                <Status color="green">Concluído</Status>
-              </td>
-            </tr>
+            {cycles
+              .map((cycle) => (
+                <tr key={cycle.id}>
+                  <td>{cycle.subject}</td>
+                  <td>{cycle.minutes}m</td>
+                  <td>
+                    {formatDistanceToNow(cycle.startDate, {
+                      addSuffix: true,
+                      locale: ptBR,
+                    })}
+                  </td>
+                  <td>
+                    {cycle.finishedDate && (
+                      <Status color="green">Concluído</Status>
+                    )}
+                    {cycle.interruptedDate && (
+                      <Status color="red">Interrompido</Status>
+                    )}
+                    {!cycle.finishedDate && !cycle.interruptedDate && (
+                      <Status color="yellow">Em andamento</Status>
+                    )}
+                  </td>
+                </tr>
+              ))
+              .reverse()}
           </tbody>
         </table>
       </TableContainer>
